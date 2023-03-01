457c863008ecc5c1a0c4e310d433f968b4d727b7
Emeson12345678910 <124007178+Emeson12345678910@users.noreply.github.com>
2023-03-01 10:58:21 -0600

    Add files via upload

---
+++ LABORATORIO1+EmersonEliMendozaLemus/Entidade/Personas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LABORATORIO1_EmersonEliMendozaLemus.Entidade
+{
+    public class Personas
+    {
+    public string Nombre { get; set; }
+    public int Edad { get; set; }
+    public string Sexo { get; set;}
+    public int Peso { get; set;}
+    public double Altura { get; set;}
+    }
+}
---
+++ LABORATORIO1+EmersonEliMendozaLemus/Negocio/ClsPersonas.cs
@@ -0,0 +1,40 @@
+﻿using LABORATORIO1_EmersonEliMendozaLemus.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LABORATORIO1_EmersonEliMendozaLemus.Negocio
+{
+    public class ClsPersonas
+
+    {
+        public String calcularIMC(Personas personas) {
+            double pro = personas.Peso /
+             Math.Pow(personas.Altura, 2);
+                if (pro <= 24)
+                
+                {
+                    return "Tu peso es ideal";
+                } return " tu tienes Sobre peso";
+
+
+            }
+        public string EsMayorDeEdad(Personas edad)
+
+        {
+            if (edad.Edad < 18) {
+                return "Tu eres menor  de edad";
+
+            }return "Tu eres mayor de edad";
+        }
+        }
+        
+         
+        
+    }
+
